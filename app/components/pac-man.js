f95f7b0ad60a0e6a5fb2594ba5eb2b9e4dcb9b16
Miwa Yagi <miwayagi@gmail.com>
2017-05-26 15:46:59 -0400

    Pac-man computed

---
+++ app/components/pac-man.js
@@ -0,0 +1,59 @@
+import Ember from 'ember';
+import KeyboardShortcuts from 'ember-keyboard-shortcuts/mixins/component';
+
+export default Ember.Component.extend(KeyboardShortcuts, {
+  // didInsertElement: function(){
+  //   this.drawCircle();
+  // },
+
+  x: 50,
+  y: 100,
+  squareSize: 40,
+  ctx: Ember.computed(function(){
+    let canvas = document.getElementById("myCanvas");
+    let ctx = canvas.getContext("2d");
+    return ctx;
+  }),
+
+  drawCircle: function(){
+    let ctx = this.get('ctx');
+    let x = this.get('x');
+    let y = this.get('y');
+    let radius = this.get('squareSize')/2;
+
+    ctx.fillStyle = '#000';
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+    ctx.closePath();
+    ctx.fill();
+  },
+
+  clearScreen: function(){
+    let ctx = this.get('ctx');
+    let screenWidth = 800;
+    let screenHeight = 600;
+
+    ctx.clearRect(0, 0, screenWidth, screenHeight);
+  },
+
+  movePacMan: function(direction, amount){
+    this.incrementProperty(direction, amount);
+    this.clearScreen();
+    this.drawCircle();
+  },
+
+  keyboardShortcuts: {
+    up: function(){
+      this.movePacMan('y', -1 * this.get('squareSize'));
+    },
+    down: function(){
+      this.movePacMan('y', this.get('squareSize'));
+    },
+    left: function(){
+      this.movePacMan('x', -1 * this.get('squareSize'));
+    },
+    right: function(){
+      this.movePacMan('x', this.get('squareSize'));
+    },
+  }
+});
